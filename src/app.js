cb7e9f683f637918eaf541fb9b136709293c7869
Monira Sultana <msultana@shipperbee.com>
2021-04-16 13:58:14 -0400

    comment out migartion

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const {NODE_ENV} = require ('./core/config');
 const path = require('path');
 //const {Client} = require('pg');
 
-//Fred's way for micro service if multiple service are divided into apps older 
+
 // let combineApps = []
 // process.argv.forEach(function (val, index, array) {
 //   if (index >= 1) {
@@ -20,11 +20,11 @@ const path = require('path');
 (async () => {
   //logger.info(`Starting application. ENV ${NODE_ENV}. DB ${JSON.stringify(config.database)}`)
 
-  await db.migrate.latest ()               //([config.default.database])
-      .then(function() {
-          console.log(`Running seeds`)
-          db.seed.run();
-  })
+//   await db.migrate.latest ()               //([config.default.database])
+//       .then(function() {
+//           console.log(`Running seeds`)
+//           db.seed.run();
+//   })
   //await hapi.start(combineApps.map(app => path.resolve(__dirname, `./apps/${app}/src/routes`)))
   await hapi.start()
   console.log(`Starting server`)
